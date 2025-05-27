2a48f3e17a1ef9b207a9563bac9967f227f57bb0
77liliajm <liliajomaa@hotmail.fr>
2025-05-27 19:58:51 +0200

    Initial commit

---
+++ modele/Utilisateur.cs
@@ -0,0 +1,22 @@
+﻿namespace MediaTek86.modele
+{
+    /// <summary>
+    /// Classe métier représentant un utilisateur (login + mot de passe)
+    /// </summary>
+    public class Utilisateur
+    {
+        public string login { get; set; }
+        public string pwd { get; set; }
+
+        /// <summary>
+        /// Constructeur de la classe Utilisateur
+        /// </summary>
+        /// <param name="login">Identifiant de l'utilisateur</param>
+        /// <param name="pwd">Mot de passe de l'utilisateur</param>
+        public Utilisateur(string login, string pwd)
+        {
+            this.login = login;
+            this.pwd = pwd;
+        }
+    }
+}
